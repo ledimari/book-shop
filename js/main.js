a8aef6e15a5dfd7461ce686f17f722ca15e5fabf
ledimari <ledi.huliganka@gmail.com>
2020-12-15 23:31:33 +0200

    add header and section sale

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-function init() {
-  // функция вызовет сама себя как только сайт загрузиться
+const constrain = 50
+
+const transform = (x, y, el) => {
+  const box = el.getBoundingClientRect()
+
+  const rotateX = -(y - box.y - (box.height / 2)) / constrain
+  const rotateY = (x - box.x - (box.width / 2)) / constrain
+
+  const speed = el.getAttribute("data-speed")
+
+  const transformX = (window.innerWidth - e.pageX * speed) / 100
+  const transformY = (window.innerHeight - e.pageY * speed) / 100
+
+  return `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) `
 }
 
-(init)();
+const handleMouseMove = (e) => {
+  const sectionSaleImg = document.querySelectorAll(".sale-img")
+
+  sectionSaleImg.forEach((img) => {
+
+    window.requestAnimationFrame(() => {
+      img.style.transform = transform(e.clientX, e.clientY, img)
+    })
+  })
+}
+
+const init = () => {
+  const sectionSaleElement = document.getElementById("sale")
+
+  sectionSaleElement.addEventListener("mousemove", handleMouseMove)
+}
+
+(init)()
